7aa746b6ec305de730de7158e7c464988fbe2942
Kindred87 <13651460+Kindred87@users.noreply.github.com>
2022-11-12 09:04:32 -0800

    Made parameter optional, tweaked func doc

--- stack.go
+++ stack.go
@@ -6,21 +6,23 @@ import (
 )
 
 // getCallerInfo returns a string describing the file and line number of the caller for code calling this function.
-// The string is formatted as: "<file> on line <line number> ", or "<prepend value> <file> on line <line number> " if prepend is not empty.
+// The string is formatted as one of the following, depending on whether task contains a value:
+//   - "<file> on line <line number> "
+//   - "<task> called at line <line number> in <file> "
 //
 // Note that returned non-empty strings are appended with an empty space.
 // This allows the caller to safely prepend their message with %s without concern for whitespace.
 //
 // The returned string will be empty if an error occurred while resolving the call stack.
-func getCallerInfo(prepend string) string {
+func getCallerInfo(task ...string) string {
 	_, file, line, ok := runtime.Caller(2)
 
 	if !ok {
 		return ""
 	}
 
-	if prepend != "" {
-		return fmt.Sprintf("%s %s on line %d ", prepend, file, line)
+	if len(task) > 0 {
+		return fmt.Sprintf("%s called at line %d in %s ", task[0], line, file)
 	}
 
 	return fmt.Sprintf("%s on line %d ", file, line)
