5dc9a0bc17e249f2de650f768317d8612889bfaa
Kindred87 <13651460+Kindred87@users.noreply.github.com>
2022-11-12 09:11:23 -0800

    return task if given and error occurs

--- stack.go
+++ stack.go
@@ -6,18 +6,23 @@ import (
 )
 
 // getCallerInfo returns a string describing the file and line number of the caller for code calling this function.
-// The string is formatted as one of the following, depending on whether task contains a value:
+// The string is formatted as one of the following, depending on whether task is empty:
 //   - "<file> on line <line number> "
 //   - "<task> called at line <line number> in <file> "
+//
+// If task is not empty and an error occurs, the returned string will be formatted as:
+//   - "<task> "
 //
 // Note that returned non-empty strings are appended with an empty space.
 // This allows the caller to safely prepend their message with %s without concern for whitespace.
 //
-// The returned string will be empty if an error occurred while resolving the call stack.
+// The returned string will be empty if an error occurred while resolving the call stack and task is empty.
 func getCallerInfo(task ...string) string {
 	_, file, line, ok := runtime.Caller(2)
 
-	if !ok {
+	if !ok && len(task) > 0 {
+		return task[0] + " "
+	} else if !ok {
 		return ""
 	}
 
